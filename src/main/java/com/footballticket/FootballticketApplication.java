2b944556416567b1701aeea319f1dfa63a7b6a8b
Tuan Le <lctuan.0807@gmail.com>
2026-08-16 12:32:12 +0700

    init

---
+++ src/main/java/com/footballticket/FootballticketApplication.java
@@ -0,0 +1,13 @@
+package com.footballticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballticketApplication.class, args);
+	}
+
+}
