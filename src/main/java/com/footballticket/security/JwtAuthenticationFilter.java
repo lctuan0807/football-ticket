ab4662348948db28e9b3291661ed29d5606235ed
Tuan Le <lctuan.0807@gmail.com>
2026-08-17 15:43:24 +0700

    update

--- src/main/java/com/footballticket/security/JwtAuthenticationFilter.java
+++ src/main/java/com/footballticket/security/JwtAuthenticationFilter.java
@@ -2,7 +2,7 @@ package com.footballticket.security;
 
 import java.io.IOException;
 
-import org.springframework.lang.NonNull;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
