9058b8584a59293c5cae8705263f7a973efc1fa5
Tuan Le <lctuan.0807@gmail.com>
2026-08-18 15:08:07 +0700

    update reservation with compare stock

--- src/main/java/com/footballticket/repository/TicketTypeRepository.java
+++ src/main/java/com/footballticket/repository/TicketTypeRepository.java
@@ -26,6 +26,8 @@ public interface TicketTypeRepository extends JpaRepository<TicketTypeEntity, Lo
 
   @Modifying
   @Transactional
-  @Query("UPDATE TicketTypeEntity tt SET tt.availableQuantity = tt.availableQuantity - :quantity WHERE tt.id = :ticketTypeId")
+  // @Query("UPDATE TicketTypeEntity tt SET tt.availableQuantity =
+  // tt.availableQuantity - :quantity WHERE tt.id = :ticketTypeId")
+  @Query("UPDATE TicketTypeEntity tt SET tt.availableQuantity = tt.availableQuantity - :quantity WHERE tt.id = :ticketTypeId AND tt.availableQuantity >= :quantity")
   int reserve(@Param("ticketTypeId") Long ticketTypeId, @Param("quantity") int quantity);
 }
